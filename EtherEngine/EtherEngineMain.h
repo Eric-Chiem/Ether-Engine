922f3b20f374f2c11ba9768dfcf42c5fd559042d
Eric Chiem <echiem@wpi.edu>
2018-07-11 17:45:52 -0400

    Added DirectX 11 Project

---
+++ EtherEngine/EtherEngineMain.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\SceneRenderer3D.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace EtherEngine
+{
+	class EtherEngineMain : public DX::IDeviceNotify
+	{
+	public:
+		EtherEngineMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~EtherEngineMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<SceneRenderer3D> m_sceneRenderer3D;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
